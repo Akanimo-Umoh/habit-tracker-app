e464f4733d172d26ac40cdca17644135bd26f110
Akanimo-Umoh <akanumoh4@gmail.com>
2026-07-22 07:28:11 +0100

    Enhance Index component with selectable habits using Pressable and FlatList

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,17 @@
+import { useState } from "react";
 import {
   View,
   Text,
   StyleSheet,
   useWindowDimensions,
   FlatList,
+  Pressable,
 } from "react-native";
 
 export default function Index() {
   const { width } = useWindowDimensions();
   const ctnWidth = width * 0.9;
+  const [selectedId, setSelectedId] = useState<string | null>(null);
 
   const habits = [
     { id: "1", name: "Drink Water", streak: 5 },
@@ -24,15 +27,23 @@ export default function Index() {
         <FlatList
           data={habits}
           keyExtractor={(data) => data.id}
+          ItemSeparatorComponent={() => <View style={{ height: 12 }} />}
           contentContainerStyle={styles.innerCtn}
-          renderItem={({ item }) => (
-            <View style={styles.card}>
-              <View style={styles.row}>
-                <Text style={styles.title}>{item.name}</Text>
-                <Text style={styles.streak}>🔥 {item.streak}</Text>
-              </View>
-            </View>
-          )}
+          renderItem={({ item }) => {
+            const selected = selectedId === item.id;
+
+            return (
+              <Pressable
+                onPress={() => setSelectedId(item.id)}
+                style={[styles.card, selected && styles.selectedCard]}
+              >
+                <View style={styles.row}>
+                  <Text style={styles.title}>{item.name}</Text>
+                  <Text style={styles.streak}>🔥 {item.streak}</Text>
+                </View>
+              </Pressable>
+            );
+          }}
         />
       </View>
     </View>
@@ -42,22 +53,23 @@ export default function Index() {
 const styles = StyleSheet.create({
   main: { alignItems: "center", justifyContent: "center" },
   cardCtn: {
-    gap: 12,
     paddingTop: 12,
     paddingBottom: 12,
   },
-  innerCtn: { gap: 12, paddingHorizontal: 2, paddingVertical: 12 },
+  innerCtn: { paddingHorizontal: 2, paddingVertical: 12 },
   card: {
     backgroundColor: "#fff",
     padding: 16,
     borderRadius: 12,
-    // marginBottom: 12,
+    borderWidth: 2,
+    borderColor: "transparent",
     shadowColor: "#000",
     shadowOffset: { width: 0, height: 2 },
     shadowOpacity: 0.08,
     shadowRadius: 6,
     elevation: 2,
   },
+  saparatorComponent: { borderWidth: 1, borderColor: "red" },
   row: {
     flexDirection: "row",
     justifyContent: "space-between",
@@ -65,4 +77,8 @@ const styles = StyleSheet.create({
   },
   title: { fontSize: 16, fontWeight: "600" },
   streak: { fontSize: 14 },
+  selectedCard: {
+    borderColor: "#2563eb",
+    backgroundColor: "#eff6ff",
+  },
 });
